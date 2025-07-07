1bfc06db02b0361a7f25385dbf6f21503e9b9700
Sadavarte Shraddha <shraddhasadavarte24@gmail.com>
2025-07-07 18:22:13 +0530

    user authentication completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import {assets} from '../assets/assets'
-import {MenuIcon, SearchIcon, XIcon} from 'lucide-react'
+import {MenuIcon, SearchIcon, TicketPlus, User, XIcon} from 'lucide-react'
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
@@ -9,6 +9,8 @@ const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false)
     const {user} = useUser()
     const {openSignIn} = useClerk()
+
+    const naviagte = useNavigate()
   return (
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
       <Link to='/' className='max-md:flex-1'>
@@ -37,7 +39,11 @@ const Navbar = () => {
                  Login
                 </button>
             ) : (
-                <UserButton></UserButton>
+                <UserButton>
+                  <UserButton.MenuItems>
+                    <UserButton.Action label="My Bookings" labelIcon={<TicketPlus width={15}/>} onClick={()=> naviagte('/my-bookings')}/>
+                  </UserButton.MenuItems>
+                </UserButton>
             )
         }
         
