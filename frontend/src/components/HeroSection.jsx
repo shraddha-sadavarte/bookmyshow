f762a04ed95a4bbdaa3e2ad85a706f05efa3a9dc
Sadavarte Shraddha <shraddhasadavarte24@gmail.com>
2025-07-08 19:53:29 +0530

    moviecard and feature section created

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { assets } from '../assets/assets'
-import { CalendarIcon, ClockIcon } from 'lucide-react'
+import { ArrowRight, CalendarIcon, ClockIcon } from 'lucide-react'
+import {useNavigate} from 'react-router-dom';
 
 const HeroSection = () => {
+
+    const navigate = useNavigate()
   return (
     <div className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-[url("/backgroundImage.png")] 
     bg-cover bg-center h-screen'>
@@ -13,12 +16,21 @@ const HeroSection = () => {
         <div className='flex flex items-center gap-4 text-gray-300'>
             <span>Action | Adventure | Sci-Fi</span>
             <div className='flex items-center gap-1'>
-                <CalendarIcon className='w-4.5 h-4.5' /> 2025
+                <CalendarIcon className='w-4 h-4' /> 2025
             </div>
             <div className='flex items-center gap-1'>
-                <ClockIcon className='w-4.5 h-4.5' /> 2h 10m
+                <ClockIcon className='w-4 h-4' /> 2h 10m
             </div>
         </div>
+        <p className='max-w-md text-gray-300'>In a post-apocalyptic world where cities ride on wheels
+            and consume each other to survive, two people meet in London and try to stop a conspiracy.
+        </p>
+        <button className='flex items-center gap-1 px-6 py-3 text-sm bg-red-600
+        hover:bg-red-700 transition rounded-full font-medium cursor-pointer' 
+        onClick={()=>navigate('/movies')}>
+            Explore Movies
+            <ArrowRight className='w-5 h-5' />
+        </button>
     </div>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -23,19 +23,19 @@ const Navbar = () => {
       overflow-hidden transition-[width] duration-300 ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>
 
         <XIcon className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' onClick={() => setIsOpen(!isOpen)}/>
-        <Link onClick={() => {scrollTo(0,0), setIsOpen(false)}} to='/'>Home</Link>
-        <Link onClick={() => {scrollTo(0,0), setIsOpen(false)}} to='/movies'>Movies</Link>
-        <Link onClick={() => {scrollTo(0,0), setIsOpen(false)}} to='/'>Theaters</Link>
-        <Link onClick={() => {scrollTo(0,0), setIsOpen(false)}} to='/'>Releases</Link>
-        <Link onClick={() => {scrollTo(0,0), setIsOpen(false)}} to='/favourites'>Favourites</Link>
+        <Link onClick={() => {scrollTo(0,0); setIsOpen(false)}} to='/'>Home</Link>
+        <Link onClick={() => {scrollTo(0,0); setIsOpen(false)}} to='/movies'>Movies</Link>
+        <Link onClick={() => {scrollTo(0,0); setIsOpen(false)}} to='/'>Theaters</Link>
+        <Link onClick={() => {scrollTo(0,0); setIsOpen(false)}} to='/'>Releases</Link>
+        <Link onClick={() => {scrollTo(0,0); setIsOpen(false)}} to='/favourites'>Favourites</Link>
       </div>
 
       <div className='flex items-center gap-8'>
         <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer'/>
         {
             !user ? (
-                <button onClick={openSignIn} className='px-4 py-1 sm:px-7 sm:py-2 bg-primary
-                hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>
+                <button onClick={openSignIn} className='px-4 py-1 sm:px-7 sm:py-2 bg-red-500
+                hover:bg-red-600 transition rounded-full font-medium cursor-pointer'>
                  Login
                 </button>
             ) : (
