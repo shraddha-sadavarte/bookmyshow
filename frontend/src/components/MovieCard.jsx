047fd8616c1f0e8db30e7c67debab0900788fce0
Sadavarte Shraddha <shraddhasadavarte24@gmail.com>
2025-07-09 19:01:18 +0530

    trailer section added

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,6 +1,7 @@
 import { StarIcon } from 'lucide-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import TimeFormat from '../lib/TimeFormat'
 
 const MovieCard = ({movie}) => {
     const navigate=useNavigate()
@@ -16,7 +17,7 @@ const MovieCard = ({movie}) => {
       <p className='text-sm text-gray-400 mt-2'>
         {new Date(movie.release_date).getFullYear()} . 
         {movie.genres.slice(0,2).map(genre => genre.name).join(" | ")} . 
-        {movie.runtime}
+        {TimeFormat(movie.runtime)}
       </p>
 
       <div className='flex items-center justify-between mt-4 pb-3'>
