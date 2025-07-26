65ca2f1476a0e1a4d0ab64a33da387194afe8c31
Sadavarte Shraddha <shraddhasadavarte24@gmail.com>
2025-07-26 17:29:18 +0530

    add shows page added

--- frontend/src/pages/admin/AddShows.jsx
+++ frontend/src/pages/admin/AddShows.jsx
@@ -1,11 +1,107 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyShowsData } from '../../assets/assets';
+import Title from '../../components/admin/Title';
+import {CheckIcon, StarIcon} from 'lucide-react';
+import { kConverter } from '../../lib/kConverter';
 
 const AddShows = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+  const currency = import.meta.env.VITE_CURRENCY
+
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const [selectedMovies, setSelectedMovies] = useState(null);
+  const [dateTimeSelection, setDateTimeSelection] = useState({})
+  const [dateTimeInput, setDateTimeInput] = useState("")
+  const [showPrice, setShowPrice] = useState("")
+
+  const fetchNowPlayingMovies = async () => {
+    setNowPlaying(dummyShowsData)
+  }
+
+  const handleDateTimeAdd = () => {
+    console.log("Button clicked", dateTimeInput); 
+    if(!dateTimeInput) return
+    const [date,time] = dateTimeInput.split("T")
+    if(!date || !time) return
+
+    setDateTimeSelection((prev) => {
+      const times = prev[date] || [];
+      if(!times.includes(time)) {
+        return {...prev, [date]: [...times,time]}
+      }
+      return prev;
+    })
+    setDateTimeInput("")
+  }
+
+  const handleRemoveTime = (date, time) => {
+    setDateTimeSelection((prev) => {
+      const filteredTimes = prev[date].filter((t) => t !== time)
+      if(filteredTimes.length === 0){
+        const {[date]: _, ...rest} = prev
+        return rest;
+      }
+      return {
+        ...prev,
+        [date]:filteredTimes,
+      }
+    })
+  }
+
+  useEffect(() => {
+    fetchNowPlayingMovies();
+  },[])
+
+  return nowPlaying.length > 0 ? (
+    <>
+      <Title text1="Add" text2="Shows" />
+      <p className='mt-10 text-lg font-medium'>NOw Playing Movies</p>
+      <div className='overflow-x-auto pb-4'>
+        <div className='group flex flex-wrap gap-4 mt-4 w-max'>
+          {nowPlaying.map((movie) => (
+            <div key={movie.id} className={`relative max-w-40 cursor-pointer group-hover:not-hover:opacity-40 hover:translate-y-1 transition
+              duration-300`} onClick={() => setSelectedMovies(movie.id)}>
+                <div className='relative rounded-lg overflow-hidden'>
+                  <img src={movie.poster_path} alt='' className='w-full object-cover brightness-90' /> 
+                  <div className='text-sm flex items-ceter justify-between p-2 bg-black/70 w-full absolute bottom-0 left-0'>
+                    <p className='flex items-center gap-1 text-gray-400'>
+                      <StarIcon className='w-4 h-4 text-red-500 fill-red-500' />
+                      {movie.vote_average.toFixed(1)}
+                    </p>
+                    <p className='text-gray-300'>{kConverter(movie.vote_count)} Votes</p>
+                  </div>
+                </div>
+                {selectedMovies === movie.id && (
+                  <div className='absolute top-2 right-2 flex items-center justify-center bg-red-500 h-6 w-6 rounded'>
+                    <CheckIcon className='w-4 h-4 text-white' strokeWidth={2.5} />
+                  </div>
+                )}
+
+                <p className='font-medium truncate'>{movie.title}</p>
+                <p className='text-gray-400 text-sm'>{movie.release_date}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/*show price input */}
+      <div className='mt-8'>
+        <label className='block text-sm font-medium mb-2'>Show Price</label>
+        <div className='inline-flex items-center gap-2 border border-gray-600 px-3 py-2 rounded-md'>
+          <p className='text-gray-400 text-sm'>{currency}</p>
+          <input min={0} type='number' value={showPrice} onChange={(e) => setShowPrice(e.target.value)} placeholder='Enter Show Price' className='outline-none' />
+        </div>
+      </div>
+
+       {/*Date & Time Selection*/}
+       <div className='mt-6'>
+        <label className='block text-sm font-medium mb-2'>Select Date and Time</label>
+        <div className='inline-flex gap-5 border border-gray-600 p-1 pl-3 rounded-lg'>
+          <input type='datetime-local' value={dateTimeInput} onChange={(e) => setDateTimeInput(e.target.value)} className='outline-none rounded-md'/>
+          <button type='button' onClick={handleDateTimeAdd} className='bg-red-500/80 text-white px-3 py-2 text-sm rounded-lg hover:bg-red-500 cursor-pointer'>Add Time</button>
+        </div>
+       </div>
+    </>
+  ) : <p>Loading...</p>
 }
 
 export default AddShows
